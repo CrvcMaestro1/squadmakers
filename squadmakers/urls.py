69112bade7c3f515111c60d67c10d7d8dd427319
Christian Vera <Crvc1998@gmail.com>
2022-05-20 14:57:25 -0500

    Fix deprecated url

--- squadmakers/urls.py
+++ squadmakers/urls.py
@@ -13,9 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 from django.views.static import serve
 
@@ -28,5 +27,5 @@ urlpatterns = [
     ), name='swagger-ui'),
     path("admin/", admin.site.urls),
     path("api/", include("transactions.urls")),
-    url(r'^static/(?P<path>.*)$', serve, {'document_root': STATIC_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': STATIC_ROOT}),
 ]
